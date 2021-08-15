9cca0fb74a61cbe67c937588dba23ee816f998a4
ensarkoras <ensarkoras@gmail.com>
2021-08-15 15:21:48 +0300

    Initial commit

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+using Newtonsoft.Json;
+
+namespace auth.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        
+        [JsonIgnore]
+        public string Password { get; set; }
+    }
+}
